efb90bd8ecca48aaeee662fc9404e9ad6a223181
Carlos França Júnior <francasense@gmail.com>
2019-10-22 10:39:29 -0300

    primeirocommit

---
+++ server.js
@@ -0,0 +1,30 @@
+//server.js
+const express = require('express');
+const cors = require('cors')
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(cors({
+  origin: 'https://orcnroll.herokuapp.com', credentials: true
+}));
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
